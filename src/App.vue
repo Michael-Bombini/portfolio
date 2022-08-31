e89763a278ab75fcbbabaa11eeac6b88ed016338
Michael Bombini <bombini.michael@gmail.com>
2022-08-31 23:10:40 +0200

    Created hero section

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TheHero />
+    <h1 class="bg-danger text-center fw-bold">WORK IN PROGRESS!</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheHero from "./components/TheHero.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TheHero,
+  },
+};
 </script>
 
 <style lang="scss">
 @import "assets/styles/main.scss";
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+body {
+  overflow-x: hidden;
 }
+
+
 </style>
---
+++ src/components/TheHero.vue
@@ -0,0 +1,135 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="row p-1 align-items-center">
+        <div class="col-sm-12 col-md-6  col-intro">
+          <h3>Hey there!</h3>
+          <h1>I'm Michael</h1>
+          <h2>A junior <span>Full-Stack</span> web developer</h2>
+          <button class="mt-2">
+            See my works
+            <span class="ms-3"
+              ><img :src="require('../assets/images/work.svg')" alt=""
+            /></span>
+          </button>
+          <button class="ms-sm-3 ms-md-0 ms-lg-4 mt-2">
+            Get in contact
+            <span class="ms-3"
+              ><img :src="require('../assets/images/deliver.svg')" alt=""
+            /></span>
+          </button>
+        </div>
+        <div class="col-md-8 col-lg-6">
+          <img :src="require('../assets/images/blob.svg')" alt="" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TheHero",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/styles/variables";
+section {
+  height: 100vh;
+  background-color: #121117;
+}
+
+.container {
+  padding: 150px 0 ;
+}
+
+svg path {
+  transition: fill 0.5s ease;
+}
+svg:hover path {
+  fill: #db2955;
+  transition: fill 0.5s ease;
+}
+
+img {
+  max-width: 100%;
+}
+
+h1 {
+  font-size: $heading-big;
+  font-weight: 700;
+  color: $primary;
+}
+
+h2 {
+  width: 20ch;
+  font-size: $heading-medium;
+  font-weight: 700;
+  color: $white;
+  margin-bottom: 2.8rem;
+}
+
+
+span {
+  color: $primary;
+}
+
+h3 {
+  font-size: $heading-small;
+  font-weight: 600;
+  color: $white;
+  margin-bottom: 1.5rem;
+  position: relative;
+}
+
+h3::before {
+  content: "";
+  display: block;
+  width: 3px;
+  height: 42px;
+  background: $primary;
+  position: absolute;
+  bottom: 0;
+  left: -5%;
+}
+
+
+
+button {
+  background-color: transparent;
+  color: $primary-light;
+  border: 0;
+  font-size: $text-small;
+  border: 1px solid $primary;
+  padding: 0.7rem 0.8rem;
+  width: 220px;
+}
+
+@media screen and (max-width : 1200px) {
+  * {
+    font-size: 89%;
+  }
+}
+
+@media screen and (max-width : 992px) {
+  .row{
+    justify-content: center;
+  }
+}
+
+@media screen and (max-width : 768px) {
+ .col-intro {
+  text-align: center;
+ }
+ h2 {
+  margin: auto;
+  margin-bottom: 1.8rem;
+ }
+}
+
+
+</style>
